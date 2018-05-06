361e70670a57d3627c7542ac3711e5b2370cda5a
Spider97 <38635175+Spider97@users.noreply.github.com>
2018-05-07 00:58:48 +0200

    Create sr.py

---
+++ sr.py
@@ -0,0 +1,31 @@
+import threading
+import random
+
+class Receiver(object):
+  def __init__(self):
+  def getMessage(self, sender):
+    message = sender.Message()
+  def sendACK(self, sender):
+  def sendMessage(self, sender):
+        ER = 5
+        if random.randint(0, 99) < ER:
+            return
+        else:
+            self.sendACK(sender)
+  def Start(self, sender):
+        while(True)
+            self.getMessage(sender)
+ 
+class Sender(object):
+    def __init__(self):
+    def Message(self):
+        return message
+    def getMessage(self, receiver):
+        if(receiver.ACK == -1) 
+            self.sendPacket(self, receiver)
+        else
+            return
+    def sendPacket(self, receiver):
+    def Start(self):
+        while (True)
+            self.getMessage(receiver) 
